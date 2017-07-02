3a7abcc572752b26c47d86ecba284b3a14093e0f
nisarg1809 <nisarg@bestvpn.top>
2017-07-02 17:59:40 +0530

    Home pagr design

--- SBC.Web/Controllers/HomeController.cs
+++ SBC.Web/Controllers/HomeController.cs
@@ -6,24 +6,12 @@ using System.Web.Mvc;
 
 namespace SBC.Web.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+        //dashboard
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
